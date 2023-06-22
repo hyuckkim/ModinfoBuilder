54476c0d4ee57891fc0ef3f3e23eb7a67147c497
hyuckkim <monkeyhyuck@naver.com>
2023-06-23 08:33:51 +0900

    add ModinfoInfo class

---
+++ ModinfoBuilder/ModinfoInfo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ModinfoBuilder;
+
+internal class ModinfoInfo
+{
+    private FileInfo file;
+    private string path;
+    public ModinfoInfo(FileInfo file, string path) 
+    {
+        this.file = file;
+        this.path = path;
+    }
+
+    public async Task<ModinfoRecord> Modify()
+    {
+        ModinfoRecord rec = (0, 0, 0, 0);
+        XmlDocument doc = GetDocumentByPath(file);
+        List<string> unusedFiles = path.GetAllSources().ToList();
+        try
+        {
+            foreach (XmlElement data in GetFileNodes(doc)
+                .Cast<XmlNode>()
+                .Select(node => (XmlElement)node))
+            {
+                var d = await ChangeResource(unusedFiles, data);
+                rec = rec.AddFileStatus(ResolveFileStatus(d, unusedFiles));
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"{file.Name}이 {e}");
+        }
+        foreach (string file in unusedFiles)
+        {
+            Console.WriteLine($"경고 : {file}이 modinfo에 없습니다.");
+        }
+        using FileStream stream = file.Open(FileMode.Create);
+        doc.Save(stream);
+
+        rec.missed = unusedFiles.Count;
+        return rec;
+    }
+
+    static XmlDocument GetDocumentByPath(FileInfo path)
+    {
+        XmlDocument doc = new();
+        FileStream stream = path.OpenRead();
+        doc.Load(stream);
+        stream.Close();
+
+        return doc;
+    }
+    static async Task<FileStatus> ChangeResource(IEnumerable<string> files, XmlElement node)
+    {
+        string checkPath = node.InnerText.ReplaceSlash();
+        string? path = files
+            .Where(p => p.EndsWith(checkPath))
+            .FirstOrDefault();
+        if (path is null) return new NotFound(checkPath);
+
+        FileStream ns = File.OpenRead(path);
+
+        string oldHash = node.GetAttribute("md5");
+
+        string hash = await MD5Util.Calculate(ns);
+        node.SetAttribute("md5", hash);
+
+        string newHash = node.GetAttribute("md5");
+
+        if (oldHash != newHash) return new Changed(checkPath, oldHash, newHash, path);
+        else return new Ignored(checkPath, path);
+    }
+    static FileStatus ResolveFileStatus(FileStatus code, List<string> unusedFiles)
+    {
+        Console.Write(code.StatusText);
+        if (code.FileExists)
+        {
+            unusedFiles.Remove(code.FullPath);
+        }
+
+        return code;
+    }
+    static XmlNodeList GetFileNodes(XmlDocument doc) => doc
+        .SelectNodes("Mod/Files/File")
+        ?? throw new Exception("유효한 modinfo 파일이 아닙니다.");
+}
--- ModinfoBuilder/Program.cs
+++ ModinfoBuilder/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System.Xml;
 using System.Security.Cryptography;
 using System.Text.RegularExpressions;
-
-MD5 md5 = MD5.Create();
+using ModinfoBuilder;
 
 if (args.Length == 0)
 {
@@ -22,100 +21,21 @@ var modinfoList =
     let folderName = Regex.Replace(path, @"[/\\][^/\\]+\..+", "")// Remove file name and use paths only
     select (modInfo, folderName);
     
+// Todo : modinfoInfo 클래스를 만들어서 분리!
 foreach ((FileInfo modInfo, string folderName) in modinfoList)
-    {
-        Console.WriteLine($"- {folderName}");
+{
+    ModinfoInfo modinfoData = new(modInfo, folderName);
 
-        var (changed, ignored, notFound, missed) = await modifyModinfo(modInfo, folderName);
+    Console.WriteLine($"- {folderName}");
 
-        Console.WriteLine($"{changed} 변경됨, {ignored} 유지됨, {notFound}, 파일 없음, {missed} modinfo에 없음");
-        Console.WriteLine();
-    }
+    var (changed, ignored, notFound, missed) = await modinfoData.Modify();
+
+    Console.WriteLine($"{changed} 변경됨, {ignored} 유지됨, {notFound}, 파일 없음, {missed} modinfo에 없음");
+    Console.WriteLine();
+}
 
 Console.Write("계속하려면 아무 키나 누르십시오...");
 Console.Read();
-
-
-async Task<ModinfoRecord> modifyModinfo(FileInfo modInfo, string path)
-{
-    ModinfoRecord rec = (0, 0, 0, 0);
-    XmlDocument doc = GetDocumentByPath(modInfo);
-    List<string> unusedFiles = path.GetAllSources().ToList();
-    try
-    {
-        foreach (XmlElement data in GetFileNodes(doc)
-            .Cast<XmlNode>()
-            .Select(node => (XmlElement)node))
-        {
-            var d = await ChangeResource(unusedFiles, data);
-            rec = rec.AddFileStatus(ResolveFileStatus(d, unusedFiles));
-        }
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine($"{modInfo.Name}이 {e}");
-    }
-    foreach (string file in unusedFiles)
-    {
-        Console.WriteLine($"경고 : {file}이 modinfo에 없습니다.");
-    }
-    using FileStream stream = modInfo.Open(FileMode.Create);
-    doc.Save(stream);
-
-    rec.missed = unusedFiles.Count;
-    return rec;
-}
-FileStatus ResolveFileStatus(FileStatus code, List<string> unusedFiles)
-{
-    Console.Write(code.StatusText);
-    if (code.FileExists)
-    {
-        unusedFiles.Remove(code.FullPath);
-    }
-
-    return code;
-}
-async Task<FileStatus> ChangeResource(IEnumerable<string> files, XmlElement node)
-{
-    string checkPath = node.InnerText.ReplaceSlash();
-    string? path = files
-        .Where(p => p.EndsWith(checkPath))
-        .FirstOrDefault();
-    if (path is null) return new NotFound(checkPath);
-
-    FileStream ns = File.OpenRead(path);
-
-    string oldHash = node.GetAttribute("md5");
-
-    string hash = await CalculateMD5ByFile(ns);
-    node.SetAttribute("md5", hash);
-
-    string newHash = node.GetAttribute("md5");
-
-    if (oldHash != newHash) return new Changed(checkPath, oldHash, newHash, path);
-    else return new Ignored(checkPath, path);
-}
-XmlDocument GetDocumentByPath(FileInfo path)
-{
-    XmlDocument doc = new();
-    FileStream stream = path.OpenRead();
-    doc.Load(stream);
-    stream.Close();
-
-    return doc;
-}
-XmlNodeList GetFileNodes(XmlDocument doc) => doc
-    .SelectNodes("Mod/Files/File") 
-    ?? throw new Exception("유효한 modinfo 파일이 아닙니다.");
-
-async Task<string> CalculateMD5ByFile(FileStream stream)
-{
-    byte[] res = await md5
-    .ComputeHashAsync(stream);
-    return res
-        .Aggregate("", (acc, x) => $"{acc}{x:x2}")
-        .ToUpper();
-}
 
 internal static class Extension
 {
@@ -127,4 +47,16 @@ internal static class Extension
         => Directory.GetFiles(path, "*", SearchOption.AllDirectories)
             .Where(e => !e.EndsWith(".modinfo"))
             .Select(e => e.ReplaceSlash());
+}
+internal static class MD5Util
+{
+    static readonly MD5 md5 = MD5.Create();
+    public static async Task<string> Calculate(FileStream stream)
+    {
+        byte[] res = await md5
+        .ComputeHashAsync(stream);
+        return res
+            .Aggregate("", (acc, x) => $"{acc}{x:x2}")
+            .ToUpper();
+    }
 }
